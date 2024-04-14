55b7b5fbfd02160c0a6382229a47e6232ab048fe
Borjabs0 <146438165+Borjabs0@users.noreply.github.com>
2024-04-14 18:05:06 +0200

    Commit ejercicio 5

---
+++ src/main/java/com/borjabolufer/ejercicios/ejercicio05/Ejercicio05.java
@@ -0,0 +1,42 @@
+package com.borjabolufer.ejercicios.ejercicio05;
+
+import com.borjabolufer.Lib.LibIO;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ejercicio05 {
+ private Alumno[] alumnos;
+ private  Scanner scanner;
+
+    public Ejercicio05() {
+        solicitarDatos();
+    }
+    public void solicitarDatos(){
+        alumnos = new Alumno[2];
+        String nombre;
+        int edad;
+        float altura;
+        scanner = new Scanner(System.in);
+        for (int i = 0; i < alumnos.length; i++) {
+            System.out.println("Alumno " + (i + 1));
+            nombre = LibIO.solicitarString("Dime tu nombre", 3, 23 );
+            edad = LibIO.solicitarInt("Dime tu edad: ", 0, 99);
+            altura = LibIO.solicitarFloat("Dime tu altura: ", 0, 3);
+            alumnos[i] = new Alumno(nombre, edad, altura );
+            }
+        System.out.println(Arrays.toString(alumnos));
+
+        String alumnoMayor;
+        if (alumnos[0].getEdad() > alumnos[1].getEdad()){
+            alumnoMayor = alumnos[0].getNombre();
+            System.out.println("El alumno " + alumnoMayor + " es mayor que el alumno " + alumnos[1].getNombre());
+        } else if (alumnos[0].getEdad() ==  alumnos[1].getEdad()) {
+            System.out.println("Tienen la misma edad");
+        } else{
+            alumnoMayor = alumnos[1].getNombre();
+            System.out.println("El alumno " + alumnoMayor + " es mayor que el alumno " + alumnos[0].getNombre());
+        }
+    }
+
+}
